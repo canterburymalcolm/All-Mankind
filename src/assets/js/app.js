bd5b5d2e5f71cf9486acbc00c206737e3267218a
malcolm <canterburymalcolm@gmail.com>
2018-12-30 00:05:25 -0800

    store page added for mobile and desktop

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -17,8 +17,32 @@ require('foundation-sites');
 $(document).foundation();
 
 $(document).ready(function(){
+    //store link for mobile landing page
     $("#store").click(function(){
         console.log("clicked");
         window.location.href = 'shop.html';
     });
+
+    $("#sneakyLink").click(function(){
+        window.location.href = 'landing.html';
+    });
+    
+    //hamburg menu opening animation
+    $("#hamburg").click(function(){
+        console.log("hamburger");
+        if ($(".menu").css("display") == "none") {
+            $(".menu").css("display", "block");
+            $("#hamburg").attr("src", "../assets/img/ham2.png");
+        } else {
+            $(".menu").css("display", "none");
+            $("#hamburg").attr("src", "../assets/img/ham1.png");
+        }
+    });
+
+    $("#linkSnake").hover(
+        function() {
+            $("#storeSnake").attr("src", "../assets/img/snakeandbubble2.png");
+        }, function() {
+            $("#storeSnake").attr("src", "../assets/img/snakeandbubble.png");
+        });
 });
