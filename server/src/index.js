772bf876e1b7cd55d1e1c87fe853386a710badbd
malcolm <canterburymalcolm@gmail.com>
2020-02-02 13:16:24 -0500

    resolvers file

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,11 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({
+     typeDefs,
+     resolvers
+    });
 
 server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`);
