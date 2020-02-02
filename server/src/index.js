9fb2ee15057543ea7db5eb0cae10747661f93d9d
malcolm <canterburymalcolm@gmail.com>
2020-02-02 13:37:38 -0500

    Revert "resolvers file"
    This reverts commit 772bf876e1b7cd55d1e1c87fe853386a710badbd.

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
 
-const server = new ApolloServer({
-     typeDefs,
-     resolvers
-    });
+const server = new ApolloServer({ typeDefs });
 
 server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`);
--- server/src/resolvers.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    Query: {
-        inventory: (_, __, { datasources }) =>
-            datasources.productApi.getInventory(),
-        product: (_, { id }, { datasources }) =>
-            datasources.productApi.getProductById({ productId: id })
-    }
-};
